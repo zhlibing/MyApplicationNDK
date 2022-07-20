f0d8b98b6eacd42f266cab020968ba93d1cf6797
zlb <42637117@qq.com>
2022-07-20 20:48:48 +0800

    test Optional

--- app/src/main/java/com/example/myapplication/factoryStrategy/factoryStrategyTest.java
+++ app/src/main/java/com/example/myapplication/factoryStrategy/factoryStrategyTest.java
@@ -1,12 +1,21 @@
 package com.example.myapplication.factoryStrategy;
 
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+import java.util.Optional;
+
 public class factoryStrategyTest {
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     public static void main(String[] args) {
         System.out.println("使用近距离攻击策略");
         StrategyFactory.StrategyInstance.getStrategyFactory().getSparseArray(0).fight(new Knife());
         System.out.println("现在使用远距离攻击策略");
         StrategyFactory.StrategyInstance.getStrategyFactory().getSparseArray(1).fight(new Gun());
+        Weapon weapon = new Knife();
+        Optional.ofNullable(weapon).map(weapon1 -> new Gun()).ifPresent(weapon1 -> weapon.closeAttract());
     }
 
 }
